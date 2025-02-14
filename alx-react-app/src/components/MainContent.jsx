0bfeb8bafb45faf6e23990a3679801e27c416966
rgyslain <gislainrugira@gmail.com>
2025-02-14 18:09:05 +0200

    adddefault Footer;

--- alx-react-app/src/components/MainContent.jsx
+++ alx-react-app/src/components/MainContent.jsx
@@ -1,3 +1,13 @@
-<main>
-  <p>I love to visit New York, Paris, and Tokyo.</p>
-</main>
+
+function MainContent() {
+    return(
+        <main>
+        <p>
+        I love to visit New York, Paris, and Tokyo.
+        </p>
+        </main>
+    );
+
+    
+}
+export default MainContent;

acd7d26d55b6919343485790ed9ae881414bdb91
rgyslain <gislainrugira@gmail.com>
2025-02-23 17:07:08 +0200

    final touches

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -1,18 +1,32 @@
-// Navbar.jsx
+
 import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
+    const navbarStyle = {
+        backgroundColor: 'rgba(140, 152, 151, 0.89)',
+        display: 'flex',
+        justifyContent: 'space-between',
+        padding: '1rem'
+        
+    };
+
+    const linkStyle = {
+        color: 'rgb(0, 0, 0)',
+        textDecoration: 'none',
+        padding: '0.5rem 1rem'
+    };
     return (
-        <nav>
-            <ul>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/about">About</Link></li>
-                <li><Link to="/services">Services</Link></li>
-                <li><Link to="/contact">Contact</Link></li>
+        <nav style={navbarStyle}>
+            <ul style={{ display: 'flex', listStyleType: 'none', margin: 0, padding: 0 }}>
+                <li><Link to="/"  style={linkStyle}>Home</Link></li>
+                <li><Link to="/about" style={linkStyle}>About</Link></li>
+                <li><Link to="/services" style={linkStyle}>Services</Link></li>
+                <li><Link to="/contact" style={linkStyle}>Contact</Link></li>
             </ul>
         </nav>
     );
 };
 
 export default Navbar;
+

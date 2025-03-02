4e2e7f9d58430cbb5e471a9660b72d3be624ce4c
rgyslain <gislainrugira@gmail.com>
2025-03-02 15:50:41 +0200

    now

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,18 +1,21 @@
 import AddRecipeForm from './components/AddRecipeForm'
 import RecipeList from './components/RecipeList'
 import './App.css'
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
+import RecipeDetails from './components/RecipeDetails';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-   
-    
-    
-    
-    </>
-    
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/recipe" element={<RecipeDetails />} />
+      </Routes>
+    </Router>
   )
 }
 

f4dee4b9e24bce79e9841d565366fc12d928ceb8
rgyslain <gislainrugira@gmail.com>
2025-03-08 18:43:45 +0200

    blloog

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,22 +1,45 @@
-import React from "react"
+import React from "react";
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route, Routes,Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Link, useParams } from 'react-router-dom';
 import Profile from "./components/Profile";
 
-function App() {
-  
+const BlogPost = () => {
+  const { id } = useParams();
+  return <h3>Blog Post ID: {id}</h3>;
+};
 
+function App() {
   return (
-    <>
-     <Router>
-          <Routes>
-            <Route path="/" element={<div>Home</div>} />
-            <Route path="/blog/*" element={<Profile />} />
-          </Routes>
-        </Router>
-      
-    </>
-  )
+    <Router>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/blog/2">Blog Post 2</Link>
+          </li>
+          <li>
+            <Link to="/blog/1">Blog Post 1</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Routes>
+        {/* Home route */}
+        <Route path="/" element={<div>Home Page</div>} />
+        
+        {/* Blog parent route with nested routes */}
+        <Route path="/blog/*" element={<Profile />}>
+          {/* Nested blog post route */}
+          <Route path=":id" element={<BlogPost />} />
+          
+          {/* Default nested route */}
+          <Route index element={<div>Select a blog post</div>} />
+        </Route>
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
--- react-router-advanced/src/components/Profile.jsx
+++ react-router-advanced/src/components/Profile.jsx
@@ -1,10 +1,7 @@
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route, Link, useMatch, Routes, useParams } from 'react-router-dom';
 
-const BlogPost = () => {
-  const { id } = useParams(); // Changed from userId to id to match route parameter
-  return <h3>Blog Post ID: {id}</h3>; //["/blog/:id", "BlogPost"]
-};
+
 
 function Profile() {
   const match = useMatch("/blog/:id");
@@ -26,24 +23,17 @@ function Profile() {
             </li>
           </div>
         )}
-        <li>
-          <Link to="/blog/2">Blog Post 2</Link>
-        </li>
-        <li>
-          <Link to="/blog/1">Blog Post 1</Link>
-        </li>
+       
       </ul>
 
       <Routes>
-        {/* Main blog post route */}
-        <Route path="/blog/:id" element={<BlogPost />} />
+        
         
         {/* Nested routes */}
         <Route path={`${path}/ProfileDetails`} element={<div>Profile Details</div>} />
         <Route path={`${path}/ProfileSettings`} element={<div>Profile Settings</div>} />
         
-        {/* Default route */}
-        <Route index element={<div>Please select an option.</div>} />
+        
       </Routes>
     </div>
   );

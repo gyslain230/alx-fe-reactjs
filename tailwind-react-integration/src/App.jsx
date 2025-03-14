f8006b45ab578d3bed5c7bd4644f5d762ba9c984
rgyslain <gislainrugira@gmail.com>
2025-03-14 17:15:38 +0200

    adding the responsive design

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -1,14 +1,12 @@
-import UserProfile from './components/UserProfile'
-import './App.css'
+import UserProfile from "./components/UserProfile";
+import "./App.css";
 
 function App() {
-  
-
   return (
     <>
-      <UserProfile/>
+      <UserProfile />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
